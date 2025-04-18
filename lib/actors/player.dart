29ffb0d068313c626ac14c355225e6849ba5b7d4
Neutrino-18 <raghavdave.2607@gmail.com>
2025-04-18 14:32:17 +0530

    Adding Movement

--- lib/actors/player.dart
+++ lib/actors/player.dart
@@ -2,21 +2,35 @@ import 'dart:async';
 
 import 'package:flame/components.dart';
 import 'package:flame_new/pixel_adventure.dart';
+import 'package:flutter/services.dart';
+// import 'package:flutter/src/services/hardware_keyboard.dart';
+// import 'package:flutter/src/services/keyboard_key.g.dart';
 
 enum PlayerState { idle, running }
 
+enum PlayerDirection { left, right, none }
+
 class Player extends SpriteAnimationGroupComponent
-    with HasGameRef<PixelAdventure> {
+    with HasGameRef<PixelAdventure>, KeyboardHandler {
   final String character;
   Player({super.position, required this.character});
   late final SpriteAnimation idleAnimation;
   late final SpriteAnimation runningAnimation;
   final double idleTime = 0.05;
 
+  PlayerDirection playerDirection = PlayerDirection.none;
+  double moveSpeed = 100;
+  Vector2 velocity = Vector2.zero();
   @override
   FutureOr<void> onLoad() {
     _loadAllAnimations();
     return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    _updatePlayerMovement(dt);
+    super.update(dt);
   }
 
   void _loadAllAnimations() {
@@ -46,5 +60,26 @@ class Player extends SpriteAnimationGroupComponent
         textureSize: Vector2.all(32),
       ),
     );
+  }
+
+  void _updatePlayerMovement(double dt) {
+    double dirX = 0.0;
+    switch (playerDirection) {
+      case PlayerDirection.left:
+        dirX -= moveSpeed;
+        current = PlayerState.running;
+        break;
+      case PlayerDirection.right:
+        dirX += moveSpeed;
+        current = PlayerState.running;
+        break;
+      case PlayerDirection.none:
+        current = PlayerState.idle;
+        break;
+      default:
+    }
+
+    velocity = Vector2(dirX, 0.0);
+    position += velocity * dt;
   }
 }
