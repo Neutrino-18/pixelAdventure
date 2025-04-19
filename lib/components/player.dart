d972226fbd6e03e369ea890e6afaa137bd58b672
Neutrino-18 <raghavdave.2607@gmail.com>
2025-04-19 20:24:17 +0530

    Added gravity and vertical collisions along with platform specific collisions

--- lib/components/player.dart
+++ lib/components/player.dart
@@ -17,12 +17,21 @@ class Player extends SpriteAnimationGroupComponent
   Player({super.position, this.character = ninjaFrog});
   late final SpriteAnimation idleAnimation;
   late final SpriteAnimation runningAnimation;
+  //
   final double idleTime = 0.05;
   double horizontalDirection = 0;
   List<CollisionBlock> collisionBlocks = [];
-
+  //
+  bool isOnGround = false;
+  bool hasJumped = false;
+  //
   double moveSpeed = 150;
   Vector2 velocity = Vector2.zero();
+  //
+  final double _gravity = 9.8;
+  final double _jumpForce = 320;
+  final double _terminalVelocity = 300;
+  //
   @override
   FutureOr<void> onLoad() {
     debugMode = true;
@@ -35,6 +44,8 @@ class Player extends SpriteAnimationGroupComponent
     _updatePlayerState();
     _updatePlayerMovement(dt);
     _checkHorizontalCollisions();
+    _applyGravity(dt);
+    _checkVerticalCollisions();
     super.update(dt);
   }
 
@@ -48,6 +59,8 @@ class Player extends SpriteAnimationGroupComponent
     final isRightKeyPressed =
         keysPressed.contains(LogicalKeyboardKey.keyD) ||
         keysPressed.contains(LogicalKeyboardKey.arrowRight);
+
+    hasJumped = keysPressed.contains(LogicalKeyboardKey.space);
 
     horizontalDirection += isLeftKeyPressed ? -1 : 0;
     horizontalDirection += isRightKeyPressed ? 1 : 0;
@@ -100,22 +113,71 @@ class Player extends SpriteAnimationGroupComponent
   }
 
   void _updatePlayerMovement(double dt) {
+    if (hasJumped && isOnGround) {
+      _playerJump(dt);
+    }
+    if (velocity.y > _gravity) isOnGround = false;
+
     velocity.x = horizontalDirection * moveSpeed;
     position.x += velocity.x * dt;
   }
 
+  void _playerJump(dt) {
+    velocity.y = -_jumpForce;
+    position.y += velocity.y * dt;
+    isOnGround = false;
+    hasJumped = false;
+  }
+
   void _checkHorizontalCollisions() {
-    for (var block in collisionBlocks) {
+    for (final block in collisionBlocks) {
       // handle collisions
       if (!block.isPlatform) {
         if (checkCollision(this, block)) {
           if (velocity.x > 0) {
             velocity.x = 0;
             position.x = block.x - width;
+            break;
           }
           if (velocity.x < 0) {
             velocity.x = 0;
             position.x = block.x + block.width + width;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  void _applyGravity(double dt) {
+    velocity.y += _gravity;
+    velocity.y = velocity.y.clamp(-_jumpForce, _terminalVelocity);
+    position.y += velocity.y * dt;
+  }
+
+  void _checkVerticalCollisions() {
+    for (final block in collisionBlocks) {
+      if (block.isPlatform) {
+        if (checkCollision(this, block)) {
+          if (velocity.y > 0) {
+            velocity.y = 0;
+            position.y = block.y - width;
+            isOnGround = true;
+            break;
+          }
+        }
+      } else {
+        if (checkCollision(this, block)) {
+          if (velocity.y > 0) {
+            velocity.y = 0;
+            position.y = block.y - height;
+            isOnGround = true;
+            break;
+          }
+          if (velocity.y < 0) {
+            velocity.y = 0;
+            position.y = block.y + block.height;
+            break;
           }
         }
       }
--- lib/components/utils.dart
+++ lib/components/utils.dart
@@ -9,9 +9,12 @@ bool checkCollision(player, block) {
   final blockWidth = block.width;
   final blockHeight = block.height;
 
-  final fixedX = player.scale.x < 0 ? playerX - playerWidth : playerX;
+  /* fixing the x because, when the player's direction(scale) changes, the coordinates also changes and hence the player collision happens with the other side of player rather than the expected side */
 
-  return (playerY < blockY + blockHeight &&
+  final fixedX = player.scale.x < 0 ? playerX - playerWidth : playerX;
+  final fixedY = block.isPlatform ? playerY + playerHeight : playerY;
+  // returning true when the player's dimensions exceeds(is same as) the dimensions of block otherwise false
+  return (fixedY < blockY + blockHeight &&
       playerY + playerHeight > blockY &&
       fixedX < blockX + blockWidth &&
       fixedX + playerWidth > blockX);
